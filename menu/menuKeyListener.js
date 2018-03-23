ec6d96130e6c2fe3dcc9d5862d44d45789843350
realdennis <evolon688@gmail.com>
2018-03-24 05:55:24 +0800

    remove redundant

--- menu/menuKeyListener.js
+++ menu/menuKeyListener.js
@@ -1,25 +1,16 @@
 //let keypress = require('keypress');
 let menu = require('./menu.js');
-
 exports.all = function(index){
-    //let index = 0;
     menu.render(index);
-    function checkIndexRange(index){
-        return (index+4)%4;
-    }
-    //keypress(process.stdin);
-    //process.stdin.setRawMode(true);
-    //process.stdin.resume();
+
     process.stdin.on('keypress',function arrow(ch,key){
         switch(key.name){
             case 'up':
-                index--;
-                index = checkIndexRange(index)
+                index==0?index=0:index--;
                 menu.render(index);
                 break;
             case 'down':
-                index++;
-                index = checkIndexRange(index);
+                index==3?index=3:index++;
                 menu.render(index);
                 break;
             case 'backspace':
