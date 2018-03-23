216b2cc96258c3ba91a888729e28dc125a16fdf0
realdennis <evolon688@gmail.com>
2018-03-24 00:58:24 +0800

    fix the keyword input bug

--- menu/menu.js
+++ menu/menu.js
@@ -1,21 +1,20 @@
-let xvideo = require('../xvideo/ReqParse.js')
 let lis = require('../xvideo/listen.js')
 
 console.reset =  function () {
   return process.stdout.write('\033c');
 };
-const menu = {
-    0:'Watch the home page',
-    1:'Input your keyword to find',
-    2:'Favorite',
-    3:'Exit'
-};
 
-exports.render = function(index){
+exports.render = function(index){	
+	const menu = {
+	    0:'Watch the home page',
+	    1:'Input your keyword to find',
+	    2:'Favorite',
+	    3:'Exit'
+	};
 	console.reset();
 	for(i in menu){
-		if(i == index) tab = '->  ';
-		else tab = '    '
+		if(i == index) tab = '->	';
+		else tab = '  	'
 	    console.log(tab+menu[i]);
 	}
 };
@@ -25,7 +24,7 @@ exports.choose = function menu(index){
 		console.log('Goodbye!');
 		process.stdin.pause();
 	}else{
-		console.reset();
+		console.reset();		
 		switch(index){
 			case 0:
 				process.stdin.on('keypress',lis.hchoose);
@@ -33,16 +32,40 @@ exports.choose = function menu(index){
 				//function main page listener
 				break;
 			case 1:
-				lis.kkk.keyword = '';
-				lis.keywords.renderTen();
-				process.stdin.on('keypress',lis.kchoose);
-				//keyword page listener
+				process.stdin.setRawMode(false);
+				process.stdin.removeAllListeners();
+
+				var readline = require('readline');
+				var rl = readline.createInterface({
+				  input: process.stdin,
+				  output: process.stdout
+				});
+				rl.question('Give me keyword: ', answer=>{
+					//rl.removeListener('line',input);
+					lis.kkk.keyword = answer;
+					rl.close();
+				});
+				rl.on('close',function(){
+					if(lis.kkk.keyword==''){
+						console.log('keyword empty');
+						process.exit();
+					}
+					//var keypress = require('keypress');
+					process.stdin.setRawMode(true);
+					process.stdin.resume()
+					//keypress(process.stdin);
+					process.stdin.on('keypress',lis.kchoose);
+					process.stdin.on('keypress',(ch,key)=>{
+						if(key && key.ctrl &&key.name=='c'){
+							process.stdin.pause();
+						}
+					})
+					lis.keywords.renderTen();
+				})
 				break;
 			case 2:
 				//Favorite page listener
 				console.log('2');
-				break;
-			default:
 				break;
 		}
 	}
